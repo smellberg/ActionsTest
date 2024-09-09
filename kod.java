d016e2a8aa97a091e31f94c0a105669b8e2af384
smellberg <120139015+smellberg@users.noreply.github.com>
2024-09-09 13:10:43 +0200

    Create kod.java

---
+++ kod.java
@@ -0,0 +1,45 @@
+package se.dsve.movies.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import se.dsve.movies.dtos.LoginUserDto;
+import se.dsve.movies.dtos.RegisterUserDto;
+import se.dsve.movies.model.LoginResponse;
+import se.dsve.movies.model.User;
+import se.dsve.movies.services.AuthenticationService;
+import se.dsve.movies.services.JwtService;
+
+@RequestMapping("/auth")
+@RestController
+public class AuthenticationController {
+
+    private final JwtService jwtService;
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
+        // TODO: Implement function
+        User registeredUser = authenticationService.signup(registerUserDto);
+        return ResponseEntity.ok(registeredUser);
+        // return ResponseEntity.status(HttpStatus.CREATED).body(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
+        // TODO: Implement function
+        User authenticatedUser = authenticationService.authenticate(loginUserDto);
+        String token = jwtService.generateToken(authenticatedUser); // Generera en JWT-token baserad på autentiserad användare
+        LoginResponse loginResponse = new LoginResponse(token, 5000);
+        return ResponseEntity.ok(loginResponse);
+
+    }
+}
